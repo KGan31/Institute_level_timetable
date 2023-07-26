1672724446d57e38f260c26bc8d9ed65130c9ac4
unknown <mihirgrathod23@gmail.com>
2023-07-26 21:11:06 +0530

    Added carousel on the home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../../App.css';
 import BlogList from './BlogList';
 import useFetch from './useFetch';
@@ -7,7 +7,7 @@ export default function Home(){
     const {data: blogs, isPending, error} = useFetch("http://localhost:5000/api/events");
     return(
         <>
-        <div className='home'>
+        <div className='home min-h-screen'>
             {/* <h1 style={color='black'}>All Events</h1> */}
             {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
